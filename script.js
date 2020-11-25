4fbb64c8de0f7444aace2e120729bb71578b9fdd
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-11-25 09:05:45 -0500

    add random time and random holes

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
 function randTime(min,max){
     return Math.round(Math.random()*(max - min) + min);
+}
+
+function randHole(holes){
+    //find random index
+    const idx = Math.floor(Math.random()*holes.length);
+    const hole = holes[idx];
+    if (hole===lastHole){
+        console.log('Same hole!')
+        return randHole(holes);
+    }
+    lastHole = hole;
+    return hole;
 }
