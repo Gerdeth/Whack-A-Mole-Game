572a1301efd1a1bf527a8da6acf3e2f6fa430887
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-11-25 12:05:22 -0500

    game over

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
 let timeUp =false;
+let score =0;
+const message= document.querySelector('.over');
+
+
 
 function randTime(min,max){
     return Math.round(Math.random()*(max - min) + min);
@@ -13,7 +17,6 @@ function randHole(holes){
     const idx = Math.floor(Math.random()*holes.length);
     const hole = holes[idx];
     if (hole===lastHole){
-        console.log('Same hole!')
         return randHole(holes);
     }
     lastHole = hole;
@@ -21,7 +24,7 @@ function randHole(holes){
 }
 
 function molePop(){
-    const time = randTime(200, 1000)
+    const time = randTime(600, 1000)
     const hole = randHole(holes);
     hole.classList.add('up');
     setTimeout(()=>{
@@ -30,9 +33,27 @@ function molePop(){
     },time);
   
 }
-function handleStart(){
+
+
+function startGame(){
    scoreBoard.textContent = 0; 
+   score = 0;
    timeUp= false;
    molePop();
-  setTimeout(()=> timeUp = true, 1000);
+  setTimeout(()=> timeUp = true, 10000);
+ 
+  
 }
+
+function wack(e){ 
+    if (!e.isTrusted) return; //low key authenticatio of event
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+    
+}
+// function gameOver(){
+//     console.log('done');
+//     //  message.classList.remove('message');   
+//  }
+moles.forEach(mole=>mole.addEventListener('click',wack));
